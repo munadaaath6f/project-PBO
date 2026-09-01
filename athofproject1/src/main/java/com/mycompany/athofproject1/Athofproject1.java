8f44b2c6d543490d7bdf82414a4553014af686b3
Munada Athof <munadaathof.f@gmail.com>
2026-09-01 13:53:57 +0700

    inital commit

---
+++ athofproject1/src/main/java/com/mycompany/athofproject1/Athofproject1.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.athofproject1;
+
+/**
+ *
+ * @author munad
+ */
+public class Athofproject1 {
+
+    public static void main(String[] args) {
+        System.out.println("Nama  : Athof");
+        System.out.println("NPM   : 26028");
+        System.out.println("Alama : Dieng_kulon");
+        System.out.println("No_HP : 081567890809");
+    }
+}
