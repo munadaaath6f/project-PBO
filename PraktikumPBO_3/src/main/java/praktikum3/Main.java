b867b9e2b4958c19f714cbce15a1a5a03c4b8e6e
Munada Athof <munadaathof.f@gmail.com>
2026-09-07 22:48:08 +0700

    update tugas minggu 2

---
+++ PraktikumPBO_3/src/main/java/praktikum3/Main.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package praktikum3;
+
+/**
+ *
+ * @author munad
+ */
+public class Main {
+    public static void main(String[] args) {
+        Hewan kucing = new Hewan("Mimi", 3);
+        kucing.suara();
+        kucing.info();
+    }
+}
